34f2b3f39cf128dbeca64c7c9664e072659dd113
JacobGeorgeMathew <152091838+JacobGeorgeMathew@users.noreply.github.com>
2025-09-11 19:55:12 +0530

    Modified sql queries to improve attendance read an write operation also changed the name of class table to batch

--- student_management.sql
+++ student_management.sql
@@ -12,13 +12,34 @@ CREATE TABLE branch (
     department_head VARCHAR(100)
 );
 
+-- Subject Table
+CREATE TABLE subject (
+    subject_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    code VARCHAR(20) UNIQUE NOT NULL,
+    branch_id INT,
+    FOREIGN KEY (branch_id) REFERENCES branch(branch_id)
+);
+
+-- Semester Table
+CREATE TABLE semester (
+    semester_id INT PRIMARY KEY AUTO_INCREMENT,
+    sem_num INT,
+    branch_id INT,
+    subject_id INT,
+    FOREIGN KEY (branch_id) REFERENCES branch(branch_id),
+    FOREIGN KEY (subject_id) REFERENCES subject(subject_id)
+);
+
 -- Class Table
-CREATE TABLE class (
-    class_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE batch (
+    batch_id INT PRIMARY KEY AUTO_INCREMENT,
     branch_id INT,
     name VARCHAR(100) NOT NULL,
-    semester INT,
-    FOREIGN KEY (branch_id) REFERENCES branch(branch_id)
+    started YEAR,
+    semester_id INT,
+    FOREIGN KEY (branch_id) REFERENCES branch(branch_id),
+    FOREIGN KEY (semester_id) REFERENCES semester(semester_id)
 );
 
 -- Student Table
@@ -26,13 +47,13 @@ CREATE TABLE student (
     student_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     branch_id INT,
+    batch_id INT,
     roll_number VARCHAR(50) UNIQUE NOT NULL,
-    class_id INT,
     email VARCHAR(100) UNIQUE,
     ph_no VARCHAR(20),
     password VARCHAR(255) NOT NULL,
     FOREIGN KEY (branch_id) REFERENCES branch(branch_id),
-    FOREIGN KEY (class_id) REFERENCES class(class_id)
+    FOREIGN KEY (batch_id) REFERENCES batch(batch_id)
 );
 
 -- Teacher Table
@@ -44,16 +65,6 @@ CREATE TABLE teacher (
     password VARCHAR(255) NOT NULL
 );
 
--- Subject Table
-CREATE TABLE subject (
-    subject_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    code VARCHAR(20) UNIQUE NOT NULL,
-    branch_id INT,
-    FOREIGN KEY (branch_id) REFERENCES branch(branch_id)
-);
-
-
 
 CREATE TABLE subject_cluster (
     teacher_id INT,
@@ -61,26 +72,38 @@ CREATE TABLE subject_cluster (
     PRIMARY KEY (teacher_id,subject_id),
     FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id),
     FOREIGN KEY (subject_id) REFERENCES subject(subject_id)
-) 
+); 
+
+CREATE TABLE allotted_batch (
+    teacher_id INT,
+    batch_id INT,
+    PRIMARY KEY (teacher_id,class_id),
+    FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id),
+    FOREIGN KEY (batch_id) REFERENCES batch(batch_id)
+); 
 
 -- Attendance Table
-CREATE TABLE attendance (
-    attendance_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE attendance_resource (
+    attendance_res_id INT PRIMARY KEY AUTO_INCREMENT,
+    batch_id INT,
+    subject_id INT,
     attendance_date DATE NOT NULL,
-    status TINYINT(1) NOT NULL
-    marked_at DATETIME DEFAULT NOW() ON UPDATE NOW()
+    hour TINYINT,
+    marked_at DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    FOREIGN KEY (batch_id) REFERENCES batch(batch_id),
+    FOREIGN KEY (subject_id) REFERENCES subject(subject_id),
+    UNIQUE KEY uq_session (batch_id, subject_id, attendance_date, hour)
 );
 
 -- Attendance Details Table
-CREATE TABLE attendance_detail (
-    attendance_detail_id INT PRIMARY KEY AUTO_INCREMENT,
-    attendance_id INT,
-    subject_id INT,
-    hour TINYINT,
+CREATE TABLE attendance_info (
+    attendance_id INT PRIMARY KEY AUTO_INCREMENT,
+    attendance_res_id INT,
     student_id INT,
-    FOREIGN KEY (attendance_id) REFERENCES attendance(attendance_id),
-    FOREIGN KEY (subject_id) REFERENCES subject(subject_id),
-    FOREIGN KEY (student_id) REFERENCES student(student_id)
+    status TINYINT(1) NOT NULL,
+    FOREIGN KEY (attendance_res_id) REFERENCES attendance_resource(attendance_res_id),
+    FOREIGN KEY (student_id) REFERENCES student(student_id),
+    UNIQUE KEY uq_attendance (attendance_res_id, student_id)
 );
 
 -- Result Table
